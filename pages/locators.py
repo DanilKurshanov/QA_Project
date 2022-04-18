5bc090cd93a97bb19f5aa13964dc1ea5bfbced17
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-18 10:31:09 +0300

    Add New Class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+
+    PRICE_PRODUCTS_IN_BASKET = (By.CSS_SELECTOR, "div[class='basket-mini pull-right hidden-xs']")
+    PRICE_OF_PRODUCT = (By.CSS_SELECTOR, 'div.product_main p.price_color')
+
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR, '.product_main > h1')
+    # REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
