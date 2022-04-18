273c22625ec39211bda58b557e4f76316587517a
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-18 12:34:16 +0300

    New Base Method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 import math
+import time
 
 class BasePage():
 
@@ -13,6 +17,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_url_login_correct(self):
         try:
@@ -29,12 +40,21 @@ class BasePage():
             return False
         return True
 
-    def is_prise_correct(self, how1, price_in_basket, how2, price_of_product):
+    def is_not_element_present(self, how, what, timeout=4):
         try:
-            self.browser.find_element(how1, price_in_basket).text[17:23] == self.browser.find_element(how2, price_of_product).text, 'Price not correct'
-        except:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
+
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
