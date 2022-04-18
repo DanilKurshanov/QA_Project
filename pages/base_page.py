942a2eaddc860d4b5e44da1c69fbf113b626fd19
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-18 10:30:52 +0300

    Add New Method for ProductPageTests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
+
+import math
 
 class BasePage():
 
@@ -13,7 +16,7 @@ class BasePage():
 
     def is_url_login_correct(self):
         try:
-            driver.switch_to_window(driver.window_handles[0])
+            self.browser.switch_to_window(self.browser.window_handles[0])
             'login' in self.browser.current_url
         except NotCorrectUrl:
             return False
@@ -25,3 +28,24 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+
+    def is_prise_correct(self, how1, price_in_basket, how2, price_of_product):
+        try:
+            self.browser.find_element(how1, price_in_basket).text[17:23] == self.browser.find_element(how2, price_of_product).text, 'Price not correct'
+        except:
+            return False
+        return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
