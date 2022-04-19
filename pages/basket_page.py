4410d3312b06ec0c18d8aa6712ce498b607bf3e1
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-19 13:23:01 +0300

    Creat Class BasketPage Add New Methods for Basket Page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,5 +1,14 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
+from selenium.webdriver.common.by import By
 
-class ProductPage(BasePage):
-    pass
+class BasketPage(BasePage):
+
+    def should_be_basket_page_url(self):
+        assert "/basket/" in self.browser.current_url, "URL shoud contains login."
+
+    def is_empty_basket(self):
+        assert len(self.browser.find_elements(*BasketPageLocators.EMPTY_BASKET)) == 1, 'Basket is not empty'
+
+    def should_not_be_product_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), 'Basket is not empty, but should be'
