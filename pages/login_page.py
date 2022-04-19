20f4c6bb680d451c6858cc6f5c89cb0fcd91edb2
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-19 13:24:33 +0300

    Add New Method - register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,7 +12,6 @@ class LoginPage(BasePage):
         # реализуйте проверку на корректный url адрес
         assert self.is_url_login_correct(), 'Login Url is not correct'
 
-
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login Form is not presented"
@@ -20,3 +19,15 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Login Register is not presented"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.NEW_REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.NEW_REGISTRATION_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REPEAT_NEW_REGISTRATION_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
+
+
+
+
+
