f1332a00c5c4c6ccbb108a50b8ffb7514dd69544
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-19 13:25:02 +0300

    Fix errors

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,37 @@
+import pytest
+
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                    # открываем страницу
-    page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
 
-def  test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, link)
+        login_page.should_be_login_page()
+        page.should_be_login_link()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
     page.open()
     page.should_be_to_basket_link()
     page.basket_opened_from_any_page()
-    page.is_empty_basket()
+
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_basket_page_url()
+    basket_page.should_not_be_product_in_basket()
+    basket_page.is_empty_basket()
